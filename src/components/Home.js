bc17158eb902a2466844e4fbf887ccbf7bca126d
Seong Kim <runmath@gmail.com>
2023-05-23 22:01:44 -0600

    test done

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,16 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+  const h1Style = {
+    color: "firebrick"
+  };
+  return (
+    <div id="home">
+      <h1 style={h1Style}>
+        {name} is a Web Developer from {city}
+      </h1>
+    </div>
+  );
 }
 
 export default Home;
